75acfaa49389ff6db875d5ea1895b5daa190cd87
Lian <921621443@qq.com>
2022-05-18 03:55:05 +0800

    first commit!

---
+++ src/main/java/com/example/springboottest/contorller/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.example.springboottest.contorller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @since 2022/5/7 17:26
+ */
+@RestController
+public class HelloWorldController {
+
+    class Test{
+        private int m;
+        public  void fun() {
+            // some code...
+        }
+    }
+
+    @GetMapping("/hello")
+    public String helloTest(){
+        return "Hello,World!";
+    }
+
+
+}
